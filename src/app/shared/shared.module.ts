dd22f97e29e1f3a1ec009fec36f355e84db954d5
Sebastian Górski <silent8668@gmail.com>
2019-10-07 16:22:31 +0200

    Modules cleanup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AlertComponent } from './alert/alert.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
+import { DropDownDirective } from './dropdown.directive';
+
+@NgModule({
+  declarations: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropDownDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    PlaceholderDirective,
+    DropDownDirective,
+    CommonModule
+  ],
+  entryComponents: [
+    // Only for selectors created without a selector or router reference
+    // Used for our own, programmatically created components
+    AlertComponent
+  ]
+})
+export class SharedModule {}
